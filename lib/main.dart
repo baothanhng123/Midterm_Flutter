07a5a22f97c1c14c27d64dda0913ddba8e813d9d
520H0514 <520H0514@student.tdtu.edu.vn>
2025-03-30 14:29:43 +0700

    add TextInputField

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +11,84 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text("Chat GPT"),
+          centerTitle: true,
         ),
+        body: _body(), //body
       ),
     );
+  }
+}
+
+//class for body
+class _body extends StatefulWidget {
+  @override
+  State<_body> createState() => _bodyState();
+}
+
+class _bodyState extends State<_body> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Padding(
+            padding: EdgeInsets.all(8),
+            child: IntrinsicHeight(
+                child: Column(
+              children: [
+                Expanded(
+                    child: Container(
+                  child: Text(
+                    "What can I help you with",
+                    softWrap: true,
+                    overflow: TextOverflow.visible,
+                  ),
+                )),
+                Row(
+                  children: [
+                    // Icon(Icons.photo_size_select_actual_rounded),
+                    Expanded(
+                        child: Card(
+                            child: TextField(
+                      //TEXT FIELD
+                      controller: _controller,
+                      decoration: InputDecoration(
+                          hintText: "Ask anything",
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 0.5),
+                          )),
+                      maxLines: null, // allow 4 lines
+                      keyboardType:
+                          TextInputType.multiline, //enable multiline input
+                    ))),
+                    SizedBox(
+                      height: 40,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {});
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            elevation: 0,
+                            padding: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 20),
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(2)))),
+                        child: Icon(
+                          Icons.arrow_forward,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ))));
   }
 }
